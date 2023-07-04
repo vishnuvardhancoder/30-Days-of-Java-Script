d2e8b98474e16fa2914fe75bd36b98ac317706ac
Vishnu vardhan.s <vishnuvardhansmartias007@gmail.com>
2023-07-04 21:17:59 +0530

    Added Day 4

---
+++ Day-4-Control_Statements/Conditional.js
@@ -0,0 +1,20 @@
+let x = 5;
+let y = 7;
+if(x<y){ //if statements works only when the condition is true
+    console.log(x,"is lesser than",y);
+}
+else{ //else statement works when condition in if is false
+    console.log(x,"is greater than",y);
+}
+
+
+let a = 0;
+if(a<0){
+    console.log(a,"is negative value");
+}
+else if(a>0){ //else if is used to check multiple conditions..
+    console.log(a,"is positive value");
+}
+else{
+    console.log(a,"is zero");
+}
---
+++ Day-4-Control_Statements/Looping.js
@@ -0,0 +1,22 @@
+//Loop statements are used to do repeated tasks
+//for loop (initialization,condition,incrementdecrement)
+
+for(let i =0;i<=10;i++){
+    console.log(i);
+}
+
+//while loop first initialize variable provide condition and do increment or decrement
+
+let a = 1;
+while(a<=7){
+    console.log(a);
+    a++;
+}
+
+//do while - same as while except it runs one time before checking the condition
+
+let num = 0;
+do{
+    console.log(num);
+    num++;
+}while(num<=10);
