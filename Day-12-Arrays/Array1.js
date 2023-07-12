b7abe7774714350d70cd34e77002532edaae3519
Vishnu vardhan.s <vishnuvardhansmartias007@gmail.com>
2023-07-12 22:30:20 +0530

    Added Day-12

---
+++ Day-12-Arrays/Array1.js
@@ -0,0 +1,11 @@
+//in js arrays can hold multiple data types like int,string,objects and functions.
+
+let data = [1,"aa",{roll:20},function(){console.log("hi");}]
+
+console.log(data);
+
+//if you want to call the function you can call it with the index
+
+//function is 3rd index in the array
+
+data[3]();//it will call the function which will console hi.
---
+++ Day-12-Arrays/ArrayMethods.js
@@ -0,0 +1,7 @@
+let data = [1,2,3];
+console.log(data.push(4)); //push method adds elements to the last of the array.
+console.log(data.pop()); //pop mehtod removes the last element of the array.
+console.log(data.shift()); //shift method removes the first element of the array the array is shiffted one block left
+console.log(data.unshift(0)); //unshift method adds elements to the first of the array
+console.log(data.splice(1,1, 5)); //splice used to remove and add value from inbetween the array we should pass the index and count to be removed.
+console.log(data);
