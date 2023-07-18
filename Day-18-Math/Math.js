552f239a37af3f13f198525aa4e019e012e67c69
Vishnu vardhan.s <vishnuvardhansmartias007@gmail.com>
2023-07-18 19:26:41 +0530

    Added Day 18

---
+++ Day-18-Math/Math.js
@@ -0,0 +1,19 @@
+//js has math object which will allows us to do mathematical tasks.
+//All methods and properties can be used without creating a Math object first.
+//Math.propertyname
+
+console.log(Math.PI);
+console.log(Math.round(4.4));//round rounds the value up if the value is .5 or more than .5 or else it rounds down
+console.log(Math.round(4.5));//it rounds up to 5
+console.log(Math.ceil(4.6));//ceil rounds up whatever digit after the decimal is.
+console.log(Math.floor(4.6));//floor rounds down 
+console.log(Math.trunc(3.4));//it truncates the value after decimal
+console.log(Math.pow(5,2)); //it means 5 to the power of 2 which means 5*5=25
+console.log(Math.sqrt(64));//returns the square root
+console.log(Math.abs(-5.6));//absolute returns only positive values
+console.log(Math.sin(45));//returns sin value between -1 and 1
+console.log(Math.cos(45));//returns cos value
+console.log(Math.min(2,34,65,3));//min gives the minimum value among all values
+console.log(Math.max(2,34,65,3));//max gives the maximum value among all values
+console.log(Math.random());//random returns a random number between 0 to 1 where 1 is excluded
+console.log(Math.log(2));//returns the natural logarithmic value
