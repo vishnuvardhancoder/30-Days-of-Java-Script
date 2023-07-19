98f5142439f2b0b9f42d5e18f7c4cedfd8c84d78
Vishnu vardhan.s <vishnuvardhansmartias007@gmail.com>
2023-07-19 21:13:17 +0530

    Added Day-19

---
+++ Day-19-Date/Date.js
@@ -0,0 +1,11 @@
+const d = new Date();
+console.log(d.toString());
+
+// JavaScript counts months from 0 to 11:
+
+// January = 0.
+
+// December = 11.
+
+const date = new Date(2003,9,15,13,15,25,0); //(year,month,date,hour,min,second,millisecond)
+console.log(date);
