4b3f973cc17ca35f450ddbd7402cefb5e10ccecd
Vishnu vardhan.s <vishnuvardhansmartias007@gmail.com>
2023-07-20 11:27:00 +0530

    Added Day-20

--- Day-16-Classes/ClassMethod.js
+++ Day-16-Classes/ClassMethod.js
@@ -5,10 +5,13 @@ class Laptop{
         this.ram = ram;
         this.brand = brand;
     }
+    getRam(){ //this is a method inside a class which is used to return brand of the laptop
+        return this.ram;
+    }
     getBrand(){ //this is a method inside a class which is used to return brand of the laptop
         return this.brand;
     }
 }
 
 const lap1 = new Laptop(4,"dell");
-console.log(lap1.getBrand());//calling the method for lap1.
+console.log(lap1.getRam());//calling the method for lap1.
