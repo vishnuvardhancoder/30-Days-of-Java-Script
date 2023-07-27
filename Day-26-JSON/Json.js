184134dd2e5473a9b1a9ed18b83d1a409203a109
Vishnu vardhan.s <vishnuvardhansmartias007@gmail.com>
2023-07-27 17:18:01 +0530

    Added Day-26

---
+++ Day-26-JSON/Json.js
@@ -0,0 +1,26 @@
+//JSON is a format for storing and transporting data.
+//JSON is often used when data is sent from a server to a web page.
+// JSON stands for JavaScript Object Notation
+// JSON is a lightweight data interchange format
+// JSON is language independent   
+// JSON is "self-describing" and easy to understand
+
+// JSON Syntax Rules
+// Data is in name/value pairs
+// Data is separated by commas
+// Curly braces hold objects
+// Square brackets hold arrays
+
+//JSON Data - A Name and a Value
+//"firstName":"John"
+
+//JSON Objects
+// {"firstName":"John", "lastName":"Doe"}
+
+// JSON Arrays
+
+// "employees":[
+//     {"firstName":"John", "lastName":"Doe"},
+//     {"firstName":"Anna", "lastName":"Smith"},
+//     {"firstName":"Peter", "lastName":"Jones"}
+//   ]
