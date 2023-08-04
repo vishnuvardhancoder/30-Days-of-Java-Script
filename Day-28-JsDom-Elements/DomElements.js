f1dc5793214bc3b7773ad3833fd96f3ccd017699
Vishnu vardhan.s <vishnuvardhansmartias007@gmail.com>
2023-08-04 21:13:41 +0530

    Added Day-28

---
+++ Day-28-JsDom-Elements/DomElements.js
@@ -0,0 +1,14 @@
+//how to find and access HTML elements in an HTML page.
+
+// Finding HTML elements by id
+const element1 = document.getElementById("intro");
+
+// Finding HTML Elements by Tag Name
+const element2 = document.getElementsByTagName("p");
+
+//Finding HTML Elements by Class Name
+const element3 = document.getElementsByClassName("intro");
+
+//Finding HTML Elements by CSS Selectors
+const element4 = document.querySelectorAll("p.intro");
+
